c44e245740e51984f09be9f35cd8eb0f6cba0116
rogerrendons <1584@holbertonschool.com>
2020-02-06 04:01:12 +0000

    Modifications

--- 6-size.c
+++ 6-size.c
@@ -8,7 +8,8 @@ int main(void)
 {
 printf("Size of a char: %lu\n", sizeof(char));
 printf("Size of an int: %lu\n", sizeof(int));
+printf("Size of a long int:%lu\n", sizeof(long int));
+printf("Size of a long long int: %lu\n", sizeof(long long int));
 printf("Size of a float: %lu\n", sizeof(float));
-printf("Size of a long long int: %lu\n", sizeof(double));
 return (0);
 }
