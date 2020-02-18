eb4daff973619563e7edd12327115e73edacf255
rogerrendons <1584@holbertonschool.com>
2020-02-18 14:01:42 +0000

    corrections point 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,20 +6,22 @@
  */
 void puts_half(char *str)
 {
+	int i;
+	int n;
 
-	short i, n = 0;
-
+	i = 0;
+	n = 0;
 	while (str[i] != '\0')
 	{
 		i++;
-	}
-	if ((i % 2) == 0)
-	{
-		n = (i / 2);
-	}
-	else
-	{
-		n = (i - 1) / 2;
+		if ((i % 2) == 0)
+		{
+			n = (i / 2);
+		}
+		else
+		{
+			n = (i - 1) / 2;
+		}
 	}
 	while (str[n] != '\0')
 	{
