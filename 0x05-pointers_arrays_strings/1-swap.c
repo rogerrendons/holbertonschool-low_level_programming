6b84737ed0db136205e7972910fbe3ce57b7b903
rogerrendons <1584@holbertonschool.com>
2020-02-18 13:14:18 +0000

    corrections point 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * swap_int(int *a, int *b) - function that prints the square.
+ * swap_int - function that prints the square.
  * @a: number a
  * @b: number b
  * Return: Always 0.
@@ -8,6 +8,9 @@
 
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	int ch;
+
+	ch = *a;
+	*a = *b;
+	*b = ch;
 }
