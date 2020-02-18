6b0e68c6d311c6d3723f62d7a9c739a5c352f711
rogerrendons <1584@holbertonschool.com>
2020-02-18 14:43:53 +0000

    corrections point 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,26 +2,28 @@
 /**
  * puts_half - function that prints the reverse Holberton.
  * @str: caracters to evaluate
- * Return: i.
+ * Return: 0.
  */
 void puts_half(char *str)
 {
 	int i;
 	int n;
+	int length_of_the_string;
 
 	i = 0;
 	n = 0;
 	while (str[i] != '\0')
 	{
 		i++;
-		if ((i % 2) == 0)
-		{
-			n = (i / 2);
-		}
-		else
-		{
-			n = (i - 1) / 2;
-		}
+	}
+	length_of_the_string = i;
+	if ((length_of_the_string % 2) == 0)
+	{
+		n = (length_of_the_string / 2);
+	}
+	else
+	{
+		n = (length_of_the_string - 1) / 2;
 	}
 	while (str[n] != '\0')
 	{
