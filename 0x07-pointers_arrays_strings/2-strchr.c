b3b1089d9622bcbecb5fdb1d7c3f4b6a3a79ad01
rogerrendons <1584@holbertonschool.com>
2020-02-24 17:39:15 +0000

    Load 1 point

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
-#include <stdio.h>
+
 /**
- * _memcpy - check the code for Holberton School students.
+ * _strchr - check the code for Holberton School students.
  * @s: buffer copied of src.
  * @c: sourse data to copy.
  * Return: Always 0.
  */
+
 char *_strchr(char *s, char c)
 {
 	unsigned int x;
