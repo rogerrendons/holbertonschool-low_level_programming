8f49e6cb71bf21cb6525ae09f81d47948d5fb36e
rogerrendons <1584@holbertonschool.com>
2020-02-29 18:53:25 +0000

    correction item 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,15 +14,15 @@ int main(int argc, char *argv[])
 
 	int res;
 
-	if (argc <= 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	if (argc == 3)
 	{
 		res = (atoi(argv[1]) * atoi(argv[2]));
 		printf("%d\n", res);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
