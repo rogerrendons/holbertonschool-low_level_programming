84fde16b60174949d8c62f8ccb15a8914f5cc8bd
rogerrendons <1584@holbertonschool.com>
2020-02-29 17:22:09 +0000

    Reload 0

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,6 +1,13 @@
-#include "holberton.h"
+#include <stdio.h>
+/**
+ * _atoi - return of number or char.
+ * @s: character by main.
+ * Return: 1 or 0.
+ *
+*/
 
 int _atoi(char *s)
 {
+	s++;
 	return (0);
 }
