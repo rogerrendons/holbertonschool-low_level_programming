c028d421bc1d9a27eb61a26abe8a8b475443bfaf
rogerrendons <1584@holbertonschool.com>
2020-03-01 13:17:05 +0000

    reload 4 item

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,28 +12,52 @@
 
 int main(int argc, char *argv[]__attribute__((unused)))
 {
-	int valid, res, acum = 0;
+	int lx, ly, sumrec, sum = 0;
 
-	for (valid = 1; valid < argc; ++valid)
+	if (argc == 1)
 	{
-		if (isdigit(*argv[valid]) == 0)
+		printf("0\n");
+		return (0);
+	}
+	for (lx = 1; lx < argc; lx++)
+	{
+		for (ly = 0; argv[lx][ly] !='\0'; ly++)
+		{
+			if (!((argv[lx][ly] >= 48) && (argv[lx][ly] <=57)))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+
+	for (sumrec = 0; sumrec < argc; sumrec++)
+	{
+		sum = sum + (atoi(argv[sumrec]));
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
+/**
+			if (atoi((argv[positive])) > 0)
+			{
+				res = res + (atoi(argv[positive]));
+
+
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		else
 		{
 			printf("Error\n");
 			return (1);
 		}
 	}
-	if (argc <= 1)
-	{
-		printf("0\n");
-		return (0);
-	}
-	else
-	{
-		for (acum = 0; acum < argc; acum++)
-		{
-			res = res + (atoi(argv[acum]));
-		}
-		printf("%d\n", res);
-	}
+	printf("%d\n", res);
 	return (0);
-}
+*/
