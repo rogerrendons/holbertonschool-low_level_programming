754637849a852a03a9510cb878838def40a90370
rogerrendons <1584@holbertonschool.com>
2020-03-03 20:32:53 +0000

    point 2 upload

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,23 +18,20 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	else
+	ts1 = 0;
+	while (s1[ts1] != '\0')
 	{
-		ts1 = 0;
-		while (s1[ts1] != '\0')
-		{
-			ts1++;
-		}
-		ts2 = 0;
-		while (s2[ts2] != '\0')
-		{
-			ts2++;
-		}
+		ts1++;
+	}
+	ts2 = 0;
+	while (s2[ts2] != '\0')
+	{
+		ts2++;
 	}
 	concat = malloc(sizeof(char) * (ts1 + ts2));
 	if (concat == NULL)
 	{
-		concat = '\0';
+		return (NULL);
 	}
 	tconc = 0;
 	for (tconc = 0; tconc < ts1; tconc++)
