75a0486e1d686eee9013c522535cf34dfe57253c
rogerrendons <1584@holbertonschool.com>
2020-03-03 20:14:54 +0000

    point 2 upload

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *str_concat - Ceate array.
+ * @s1: string 1.
+ * @s2: string 2.
+ * Return: char.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	unsigned int ts1, ts2, tconc = 0;
+
+	if ((s1 == NULL) || (s2 == NULL))
+	{
+		return (NULL);
+	}
+	else
+	{
+		ts1 = 0;
+		while (s1[ts1] != '\0')
+		{
+			ts1++;
+		}
+		ts2 = 0;
+		while (s2[ts2] != '\0')
+		{
+			ts2++;
+		}
+	}
+	concat = malloc(sizeof(char) * (ts1 + ts2));
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+	tconc = 0;
+	for (tconc = 0; tconc < ts1; tconc++)
+	{
+		concat[tconc] = s1[tconc];
+	}
+	for (tconc = tconc; tconc < (ts1 + ts2); tconc++)
+	{
+		concat[tconc] = s2[tconc-ts1];
+	}
+
+	printf("s1: %d, s2: %d, tconc: %d\n",ts1, ts2, tconc);
+
+	return (concat);
+}
