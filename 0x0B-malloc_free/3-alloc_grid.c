3c253b4694801ea549cee1f846729994ccdc9bc4
rogerrendons <1584@holbertonschool.com>
2020-03-03 22:17:39 +0000

    point 3 upload

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+/**
+ * alloc_grid - eturns a ptr to a 2 dimen array of int
+ * @width: input value int
+ * @height: input value int
+ * Return: int
+*/
+
+int **alloc_grid(int width, int height)
+{
+
+	int **Mat;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	Mat = malloc(height * sizeof(int *));
+
+	if (!Mat)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		Mat[i] = malloc(width * sizeof(int));
+		if (Mat[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(Mat[j]);
+			}
+			free(Mat);
+
+			return (NULL);
+		}
+
+		for (j = 0; j < height; j++)
+		{
+			Mat[i][j]= 0;
+		}
+	}
+	return (Mat);
+}
