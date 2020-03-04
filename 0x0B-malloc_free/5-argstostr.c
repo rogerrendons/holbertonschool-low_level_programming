243381c9979a0a97743a6b4634a4f4c479275924
rogerrendons <1584@holbertonschool.com>
2020-03-04 18:38:41 +0000

    Point 5

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,18 +1,50 @@
+#include "holberton.h"
 #include <stdlib.h>
 /**
- * free_grid - free bidimentional
- * @grid: bidimentional matrix
- * @height: input value int
- * Return: int
-*/
+ * argstostr - printer args
+ * @ac: numbers of args
+ * @av: array of args
+ * Return: char
+ */
 
-void free_grid(int **grid, int height)
+char *argstostr(int ac, char **av)
 {
-	int j;
 
-	for (j = 0; j < height; j++)
+	char *prnargs;
+	int i, j, rec, large = 0;
+
+	if (ac == 0 || av == NULL)
 	{
-		free(grid[j]);
+		return (NULL);
 	}
-	free(grid);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			large++;
+		}
+	}
+	large = large + ac;
+
+	prnargs = malloc(large * sizeof(char) + 1);
+
+	if (prnargs == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			prnargs[rec] = av[i][j];
+			rec++;
+		}
+		if (prnargs[rec] == '\0')
+		{
+			prnargs[rec++] = '\n';
+		}
+	}
+	return (prnargs);
 }
