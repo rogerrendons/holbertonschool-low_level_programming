5fe862bd87840d186c31a423880f41f0f6e9ad62
rogerrendons <1584@holbertonschool.com>
2020-03-05 19:12:41 +0000

    upload 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,13 +6,14 @@
  * *str_concat - Ceate array.
  * @s1: string 1.
  * @s2: string 2.
+ * @n: long of string.
  * Return: char.
  */
 
-char *str_concat(char *s1, char *s2)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
-	unsigned int ts1, ts2, tconc, tconc2 = 0;
+	unsigned int ts1, ts2, tconc, tconc2, inc = 0;
 
 	if (s1 == NULL)
 	{
@@ -31,22 +32,31 @@ char *str_concat(char *s1, char *s2)
 	while (s2[ts2] != '\0')
 	{
 		ts2++;
-
 	}
-	tconc = ts1 + ts2 + 1;
+	tconc = ts1 + inc + 1;
 	concat = malloc(sizeof(char) * tconc);
 	if (concat == NULL)
 	{
 		return (NULL);
 	}
+
+
+
 	for (tconc2 = 0; tconc2 < ts1; tconc2++)
 	{
 		concat[tconc2] = s1[tconc2];
 	}
 	for (tconc2 = 0; tconc2 < ts2; tconc2++)
 	{
-		concat[tconc2 + ts1] = s2[tconc2];
+			concat[tconc2 + ts1] = s2[tconc2];
 	}
+
+		if (ts2 < n)
+		{
+			inc++;
+		}
+
+
 	concat[ts1 + ts2] = '\0';
 	return (concat);
 }
