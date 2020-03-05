2554ae011db57166faa0479c745f13eef635a7af
rogerrendons <1584@holbertonschool.com>
2020-03-05 22:00:29 +0000

    Upload 3

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char *concat;
 
-	concat = string_nconcat("Holberton ", 0, 6);
+	concat = string_nconcat("Holberton ", "School !!!", 50);
 	printf("%s\n", concat);
 	free(concat);
 	return (0);
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,47 +1,53 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * *string_nconcat - Ceate array.
+ * *str_concat - Ceate array.
  * @s1: string 1.
  * @s2: string 2.
- * @n: Number of position to print s2.
  * Return: char.
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
-	unsigned int ts1, ts2, tconc, tconc2 = 0;
+	unsigned int ts1, ts2, tconc, tconc2, T2 = 0;
 
 	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	ts1 = 0;
-	while (s1[ts1] != '\0')
-		ts1++;
-	ts2 = 0;
-	while (s2[ts2] != '\0')
-		ts2++;
-	if (n >= ts2)
 	{
-		ts1 = 0;
 		s1 = "";
 	}
-	else if ((ts1 > 0) && (ts2 > 0))
-		ts2 = n;
-	else
-		ts2 = ts2;
-	tconc = ts1 + ts2 + 1;
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	ts1 = 0;
+	while (s1[ts1] != '\0')
+	{
+		ts1++;
+	}
+	ts2 = 0;
+	while (s2[ts2] != '\0')
+	{
+		if (ts2 < n)
+			T2++;
+		ts2++;
+	}
+	tconc = ts1 + T2 + 1;
 	concat = malloc(sizeof(char) * tconc);
 	if (concat == NULL)
+	{
 		return (NULL);
+	}
 	for (tconc2 = 0; tconc2 < ts1; tconc2++)
+	{
 		concat[tconc2] = s1[tconc2];
-	for (tconc2 = 0; tconc2 < ts2; tconc2++)
+	}
+	for (tconc2 = 0; tconc2 < T2; tconc2++)
+	{
 		concat[tconc2 + ts1] = s2[tconc2];
-	concat[ts1 + ts2] = '\0';
+	}
+	concat[ts1 + T2] = '\0';
 	return (concat);
 }
