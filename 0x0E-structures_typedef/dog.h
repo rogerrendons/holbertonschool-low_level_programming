d3a4d93ed74dd4834cb587e172ae754fb8a37d89
rogerrendons <1584@holbertonschool.com>
2020-03-09 21:42:06 +0000

    Load 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,6 +4,7 @@
 
 /**
  * struct dog - Ok
+ * struct dog_t - Ok
  * @name: Name dog
  * @age: Age dog
  * @owner: Owner dog
@@ -11,13 +12,14 @@
  * Description: Longer description
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
-};
+}dog_t;
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
