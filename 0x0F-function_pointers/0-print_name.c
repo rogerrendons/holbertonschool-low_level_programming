740d53a8ddf8ee5c0effb2e0da8c954b9c005301
rogerrendons <1584@holbertonschool.com>
2020-03-11 15:22:04 +0000

    Upload Point 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - conect a pointer
+ * @name: name from  manin
+ * @f: pointer funcion.
+ *
+ * Return: Void.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
