9e39e7db4acb5aba13f87c11a2486e0bb75951a4
rogerrendons <1584@holbertonschool.com>
2020-03-15 15:20:50 +0000

    Point 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int val_int;
 
 	if (separator == NULL)
-		return;
+		separator = "";
 
 	va_start(nums, n);
 	for (x = 0; x < n; x++)
