8f733acca95c2ce08d0266243f993cc551cd606a
rogerrendons <rogerrendons@gmail.com>
2020-03-21 02:14:54 +0000

    Upload 5 Point

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,10 +8,12 @@
  * Return: m Value Counter.
 */
 
-int printbeforMain(void) __attribute__ ((constructor));
+int printbeforMain(void) __attribute__ ((constructor))
 int printbeforMain(void)
 {
-	printf ("You're beat! and yet, you must allow,\nI bore ");
+
+	printf("You're beat! and yet, you must allow,\nI bore ");
 	printf("my house upon my back!\n");
-		return (0);
+
+	return (0);
 }
