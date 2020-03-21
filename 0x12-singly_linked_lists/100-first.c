faaf3631fcaa45b06eb632a1f96d405f3a771709
rogerrendons <rogerrendons@gmail.com>
2020-03-21 01:50:44 +0000

    Upload 3 Point

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - for managment of a list
+ * @head: poniter initial
+ * @str: new string
+ * Return: m Value Counter.
+*/
+
+int main (void)
+{
+	printf ("hello\n");
+	return 0;
+}
