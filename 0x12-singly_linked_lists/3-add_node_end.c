d89b7f67aa812a259f7a0d61bac676150748a969
rogerrendons <rogerrendons@gmail.com>
2020-03-21 03:04:40 +0000

    Upload 3 Point

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,6 +16,52 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node = (list_t *) malloc(sizeof(list_t));
 	list_t *last = *head;
 
+
+	if (head == NULL)
+		return (NULL);
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	while (new_node->str[stringlen] != '\0')
+		stringlen++;
+	new_node->len = stringlen;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (NULL);
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new_node;
+
+	return (new_node);
+}
+
+
+/**
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+*/
+
+/**
+ * add_node_end - for managment of a list
+ * @head: poniter initial
+ * @str: new string
+ * Return: pointer.
+*/
+/**
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int stringlen = 0;
+	list_t *new_node = (list_t *) malloc(sizeof(list_t));
+	list_t *last = *head;
+
+
 	if (head == NULL)
 		return (NULL);
 	if (new_node == NULL)
@@ -37,3 +83,4 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	return (new_node);
 }
+*/
