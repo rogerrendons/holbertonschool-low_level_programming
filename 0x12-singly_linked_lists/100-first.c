67ec630441e96bea5da6fd00fa637fbb3e0831fb
rogerrendons <rogerrendons@gmail.com>
2020-03-21 02:29:25 +0000

    Upload 5 Point

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,12 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include "lists.h"
 
 /**
  * pbMain - for managment of a list
  * Return: 0.
 */
 
-int pbMain(void) __attribute__ ((constructor)); int pbeMain(void)
+int pbMain(void) __attribute__ ((constructor)); int pbMain(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore ");
 	printf("my house upon my back!\n");
