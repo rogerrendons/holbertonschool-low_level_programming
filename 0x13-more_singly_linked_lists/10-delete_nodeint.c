80947fb4db5c3452a854ed3b8d41f5e5247345ac
rogerrendons <rogerrendons@gmail.com>
2020-03-24 22:16:32 +0000

    Upload 9 and 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,40 +10,34 @@
  * Return: m Value Counter.
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int Cont = 0;
-	listint_t *new_node;
 	listint_t *CopyHead;
 	listint_t *BackNode = NULL;
 
 	if (!*head || !head)
-		return (NULL);
+		return (-1);
 
 	CopyHead = *head;
 
 	while (CopyHead)
 	{
-		if (Cont == idx)
+		if (Cont == index)
 		{
-			new_node = malloc(sizeof(listint_t));
-
-			if (new_node == NULL)
-				return (NULL);
-
-			new_node->n = n;
-			new_node->next = CopyHead;
-
 			if (BackNode)
-				BackNode->next = new_node;
+				BackNode->next = CopyHead->next;
 			else
-				*head = new_node;
+				*head = CopyHead->next;
 
-			return (new_node);
+			free(CopyHead);
+			return (1);
 		}
+
 		BackNode = CopyHead;
 		CopyHead = CopyHead->next;
 		Cont++;
 	}
-	return (NULL);
+
+	return (-1);
 }
