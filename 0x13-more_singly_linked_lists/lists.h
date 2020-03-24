779e6ba5d9c96784229718e4b48099612683374f
rogerrendons <rogerrendons@gmail.com>
2020-03-24 15:18:01 +0000

    Correction pint 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LISTS
+#define LISTS
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -6,12 +9,12 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
+
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
-
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -24,3 +27,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
+#endif
