48eaff600136403f84682800ebd1233322cda64a
rogerrendons <rogerrendons@gmail.com>
2020-03-27 01:56:07 +0000

    upload 3.1

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,47 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "holberton.h"
+
 /**
- * print_binary - prints the binary representation of a number.
- * @n: number.
+ * print_binary - resulonvert integer to binary.
+ *@n: is a interger.
  */
 
 int get_bit(unsigned long int n, unsigned int index)
-
 {
-	unsigned int comp, y;
-	unsigned int lop;
-	unsigned long int x = n;
-
-	for (lop = 0; x; lop++)
-	{
-		x = x >> 1;
-	}
-	lop--;
-	if (lop >= index)
-		return (-1);
-
-	for (comp = lop; comp >= 0; comp--)
-	{
-		y = n >> comp;
-		if (y & 1)
-		{
-			_putchar('1');
-			comp == index ? return (1);
-		}
-		else
-		{
-			_putchar('0');
-		}
-	}
-	if (lop == -1)
-	{
-		y = n >> comp;
-		if (y & 1)
-		{
-			_putchar('1');
-		}
-		else
-		{
-			_putchar('0');
-		}
-	}
+	unsigned int i;
+	for (i = 0; i < index; i++)
+		n = n >> 1;
+	return (n & 1);
 }
