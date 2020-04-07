0ecb274a8b0d206bc5453b6deae17af8c6986f12
rogerrendons <rogerrendons@gmail.com>
2020-04-07 23:45:57 +0000

    probe

---
+++ simple_shell/linkedlist/_strdup.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * *_strdup - Ceate array.
+ * @str: string.
+ * Return: char.
+ */
+
+char *_strdup(char *str)
+{
+
+	char *copy;
+	unsigned int i, m = 0;
+
+	if (str == NULL)
+		return (NULL);
+	else
+	{
+		i = 0;
+		while (str[i] != '\0')
+			i++;
+	}
+	i++;
+	copy = malloc(sizeof(char) * i);
+	if (copy == NULL)
+		return (NULL);
+	for (m = 0; m < i; m++)
+		copy[m] = str[m];
+	return (copy);
+}
